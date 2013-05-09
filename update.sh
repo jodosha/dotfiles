818e85e5491215f3e1bf443a51cd016d8c273901
Luca Guidi <me@lucaguidi.com>
2013-05-09 14:08:02 +0200

    Update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+DOTFILES_ROOT="$HOME/.dotfiles"
+
+ln_s () {
+  ln -sfn "$DOTFILES_ROOT/$1" "$2"
+}
+
+echo "Fetching changes from remote repository"
+  git pull origin master
+
+echo "Fetching changes from submodules"
+  git submodule update
+
+echo "Updating user defined scripts"
+  rm ~/bin/*
+  ls bin | while read script; do ln_s "bin/$script" ~/bin; done
+
+echo "Reloading the shell"
+  exec $SHELL
