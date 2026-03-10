f88dc258aa32e3d09f15d526c9eab52d0d211998
Luca Guidi <me@lucaguidi.com>
2026-03-10 11:24:32 +0100

    NeoVim Claude Preview plugin

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -4,4 +4,8 @@ return {
     dir = "/Users/jodosha/Code/jodosha/ki/nvim-ki",
     ft = "ki",
   },
+  {
+    "Cannon07/claude-preview.nvim",
+    config = function() require("claude-preview").setup() end,
+  },
 }
