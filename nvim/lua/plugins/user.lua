d054fd90829e9467ad8df6760f7e0a1431e3319d
Luca Guidi <me@lucaguidi.com>
2026-03-13 09:37:38 +0100

    Remove NeoVim plugin `Cannon07/claude-preview.nvim`

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -4,8 +4,4 @@ return {
     dir = "/Users/jodosha/Code/jodosha/ki/nvim-ki",
     ft = "ki",
   },
-  {
-    "Cannon07/claude-preview.nvim",
-    config = function() require("claude-preview").setup() end,
-  },
 }
